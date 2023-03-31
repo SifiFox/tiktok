3cd5ca4d5b37afd6ef74b3ce650dba3f9a9617e7
SifiFox <dima.antonovich.1999@gmail.com>
2023-03-31 16:32:31 +0300

    feat: make some features

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
+
+// module.exports = nextConfig
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn.pixabay.com",
+      "pixabay.com",
+      "p16-amd-va.tiktokcdn.com",
+      "image.shutterstock.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ sanity-backend/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    {
+      name: 'postetBy',
+      title: 'Posted By',
+      type: 'postedBy',
+    },
+    {
+      name: 'comment',
+      title: 'Comment',
+      type: 'string',
+    },
+  ],
+}
