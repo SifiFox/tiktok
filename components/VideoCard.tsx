fd7efa94eddf961fb4ac1873e1941418583ddbb7
SifiFox <dima.antonovich.1999@gmail.com>
2023-03-31 17:56:27 +0300

    feat: add some styles; feat: complete play/pause btns

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -90,7 +90,7 @@ const VideoCard: NextPage<IProps> = ({ post }: IProps) => {
             ></video>
           </Link>
           {isHover && (
-            <div>
+            <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-">
               {playing ? (
                 <button onClick={onVideoPress}>
                   <BsFillPauseFill className="text-black text-2xl lg:text-4xl" />
